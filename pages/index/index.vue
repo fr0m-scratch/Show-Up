8bef054d656d1ca8feaafa6aa52b671a6c3fd8e6
fr0m-scratch <jamaltheodds@gmail.com>
2022-06-17 05:42:01 +0800

    test d

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -21,7 +21,7 @@
 						userpic:"/static/Dr. Von Neumann.jpeg",
 						newstime:"日期 时间",
 						isFollow:false,
-						title:"王子殿下午茶餐厅",
+						title:"王子殿下午茶餐厅力考试",
 						titlepic:"/static/Dr. Von Neumann.jpeg",
 						support:{
 							type:"support",
