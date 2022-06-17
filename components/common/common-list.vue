2219df67c99badf11e26a67b652de9b0181bf3ec
fr0m-scratch <jamaltheodds@gmail.com>
2022-06-17 23:10:54 +0800

    8.6

--- components/common/common-list.vue
+++ components/common/common-list.vue
@@ -4,38 +4,52 @@
 		<!-- 头像 关注 -->
 		<view class="flex align-center justify-between">
 			<view class="flex align-center">
-				<image class="rounded-circle mr-2" :src="item.userpic" style="width: 65rpx; height: 65rpx;" lazy-load></image>
+				<image class="rounded-circle mr-2" :src="item.userpic" 
+				style="width: 65rpx; height: 65rpx;" lazy-load
+				@click="openSpace"></image>
 				<view>
 					<view class="font-sm" style="line-height: 1.5;">{{item.username}}</view>
 					<text class= "font-small text-muted" 
 					style="color: #9d9589; line-height: 1.5;">{{item.newstime}}</text>
 				</view>
 			</view>
-			<view class="flex align-center justify-center font rounded bg-main text-white" style="width: 90rpx; height: 50rpx;">
+			<view class="flex align-center justify-center font rounded 
+			bg-main text-white animated faster" 
+			hover-class="bounceIn" @click="follow"
+			style="width: 90rpx; height: 50rpx;">
 				关注
 			</view>
 		</view>
 		<!-- //标题 -->
-		<view class="font-sm my-1">{{item.title}}</view>
+		<view class="font-sm my-1" @click="openDetail">{{item.title}}</view>
 		<!-- //图片 -->
 		<view>
-			<image class="rounded" :src="item.titlepic" style="height: 350rpx; width: 100%;"></image>
+			<image v-if="item.titlepic"class="rounded" :src="item.titlepic" style="height: 350rpx; width: 100%;"
+			@click="openDetail"></image>
 		</view>
 		<!-- //按钮 -->
 		<view class="flex justify-center align-center">
-			<view class="flex justify-center align-center flex-1">
+			<view class="flex justify-center align-center 
+			flex-1 animated faster" hover-class="bounceIn"
+			@click="doSupport('support')">
 				<text class="gg-heart mr-2 text-main" ></text>
 				<text>{{item.support.support_count}}</text>
 			</view>
-			<view class="flex justify-center align-center flex-1">
+			<view class="flex justify-center align-center 
+			flex-1 animated faster" hover-class="bounceIn"
+			@click="doSupport('unspport')">
 				<text class="gg-smile-neutral mr-2 text-main"></text>
 				<text>{{item.support.unsupport_count}}</text>
 			</view>
-			<view class="flex justify-center align-center flex-1">
+			<view class="flex justify-center align-center 
+			flex-1 animated faster" hover-class="bounceIn"
+			@click="openDetail">
 				<text class="gg-comment mr-2 text-main"></text>
 				<text>{{item.comment_count}}</text>
 			</view>
-			<view class="flex justify-center align-center flex-1">
+			<view class="flex justify-center align-center 
+			flex-1 animated faster" hover-class="bounceIn" 
+			@click="openDetail">
 				<text class="gg-share mr-3 text-main"></text>
 				<text>{{item.share_num}}</text>
 			</view>
@@ -51,6 +65,22 @@
 			item:Object,
 			index:Number
 		},
+		methods:{
+			openSpace(){
+				console.log("打开个人空间");
+			},
+			follow(){
+				console.log("关注");
+			},
+			openDetail(){
+				console.log("详情页");
+			},
+			doSupport(type){
+				console.log(type);
+			},
+			
+				
+		}
 	}
 </script>
 
