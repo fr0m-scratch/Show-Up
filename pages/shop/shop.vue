30599e388f27a22a9fa5c53bda9ef37f57071855
jasisboredsojoinedgithub <jasminewang0913@gmail.com>
2022-07-05 14:35:44 +0800

    11.1

--- pages/shop/shop.vue
+++ pages/shop/shop.vue
@@ -1,13 +1,17 @@
 <template>
 	<view>
 		<!-- 导航 -->
-		<uni-nav-bar :border="false" :fixed="true" :statusBar="true">
-			<view class="flex align-center justify-center font-weight-bold">
-				<view class="font-lg text-main mx-1">关注</view>
-				<view class="font-lg text-muted mx-1">话题</view>
+		<uni-nav-bar :border="false" :fixed="true" :statusBar="true" @clickRight="openAddInput">
+			<view class="flex align-center justify-center font-weight-bold w-100">
+			<!-- 	<view class="font-lg text-main mx-1">关注</view> -->
+				<view class="mx-1 my-4" @click="changeTab(index)"
+				v-for="(item,index) in tabBar" :key="index"
+				:class="tabIndex === index ? 'font-lg text-main' : 'font-md text-muted'">
+					{{item.name}}
+				</view>
 				
 			</view>
-			<text slot="right" class="gg-pen"></text>
+			<text slot="right" class="gg-pen mr-2"></text>
 			
 		</uni-nav-bar>
 	</view>
@@ -16,12 +20,33 @@
 <script>
 	import uniNavBar from "@/components/uni-ui/uni-nav-bar/uni-nav-bar.vue";
 	export default {
+		components: {
+			uniNavBar
+		},
 		data() {
 			return {
+				tabIndex:0,
+				tabBar:[{
+					name:"关注"
+				},{
+					name:"话题"
+				}]
 				
 			}
 		},
 		methods: {
+			//打开发布
+			openAddInput(){
+				uni.navigateTo({
+					url: '../add-input/add-input',
+				});
+			},
+			//打开选项卡
+			changeTab(index){
+				this.tabIndex = index
+				
+			}
+			
 			
 		}
 	}
