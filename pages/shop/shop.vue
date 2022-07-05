22de36e6cd57ef3a3d9c92aed3759234d442abb7
jasisboredsojoinedgithub <jasminewang0913@gmail.com>
2022-07-05 13:49:21 +0800

    11.1

--- pages/shop/shop.vue
+++ pages/shop/shop.vue
@@ -1,10 +1,20 @@
 <template>
 	<view>
-		就来上床睡觉
+		<!-- 导航 -->
+		<uni-nav-bar :border="false" :fixed="true" :statusBar="true">
+			<view class="flex align-center justify-center font-weight-bold">
+				<view class="font-lg text-main mx-1">关注</view>
+				<view class="font-lg text-muted mx-1">话题</view>
+				
+			</view>
+			<text slot="right" class="gg-pen"></text>
+			
+		</uni-nav-bar>
 	</view>
 </template>
 
 <script>
+	import uniNavBar from "@/components/uni-ui/uni-nav-bar/uni-nav-bar.vue";
 	export default {
 		data() {
 			return {
@@ -20,3 +30,4 @@
 <style>
 
 </style>
+
