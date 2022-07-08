f999e7fb6f7b50b6fa9f618b8ea553c57d7fb5ff
jasisboredsojoinedgithub <jasminewang0913@gmail.com>
2022-07-08 21:50:28 +0800

    18.2

--- pages/user-chat/user-chat.vue
+++ pages/user-chat/user-chat.vue
@@ -3,14 +3,26 @@
 		<uni-nav-bar backgroundColor="#7b5aa6" color="#cbd3da"
 		leftIcon="back" title="username" @clickLeft="back">
 		</uni-nav-bar>
+		<view style="height: 100rpx;"
+		class="fixed-bottom flex align-center border-top bg-white">
+			<input type="text" value="" 
+			class="flex-1 rounded ml-1 bg-light"
+			style="padding: 5rpx;"/>
+			
+			<uni-icons type="paperplane" size="30" color="purple"
+			style = "width: 100rpx;"></uni-icons>
+			
+		</view>
 	</view>
 </template>
 
 <script>
+	import uniIcons from "@/components/uni-ui/uni-icons/uni-icons.vue";
 	import statusBar from "@/components/uni-ui/uni-nav-bar/uni-status-bar.vue";
 	import uniNavBar from "@/components/uni-ui/uni-nav-bar/uni-nav-bar.vue" 
 	export default {
 		components:{
+			uniIcons,
 			statusBar,
 			uniNavBar
 		},
