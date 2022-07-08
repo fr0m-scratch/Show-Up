f068635cb675de6e78a6b0e6c67ac08da487ed71
jasisboredsojoinedgithub <jasminewang0913@gmail.com>
2022-07-08 15:09:13 +0800

    16.2

--- pages/msg/msg.vue
+++ pages/msg/msg.vue
@@ -5,11 +5,28 @@
 		title="消息" leftIcon="person" rightIcon="more-filled">
 		</uni-nav-bar>
 		<!-- 消息列表 -->
-		<view class="flex align-center"></view>
-	</view>
+		<view class="flex align-center p-2 border-bottom border-light-secondary" hover-class="bg-light">
+			<image src="../../static/Dr. Von Neumann.jpeg"
+			style="height: 80rpx; width: 80rpx;"
+			class="rounded-circle mr-2"></image>
+			<view class="flex flex-column flex-1">
+				<view class="flex align-center justify-between">
+					<text class="font-md">nic</text>
+					<text class="font text-secondary">12:33</text>
+				</view>
+				<view class="flex justify-between">
+					<text class="text-secondary">neirong</text>
+					<uni-badge text="1" type="error"></uni-badge>
+				</view>
+			</view>
+
+	    </view>
+	</view>	
+	
 </template>
 
 <script>
+	import uniBadge from "@/components/uni-ui/uni-badge/uni-badge.vue";
 	import uniNavBar from "@/components/uni-ui/uni-nav-bar/uni-nav-bar.vue";
 	
 	export default {
@@ -24,7 +41,8 @@
 			}
 		},
 		components:{
-			uniNavBar
+			uniNavBar,
+			uniBadge
 		},
 		 methods:{
 			change: function(){
